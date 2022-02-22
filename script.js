9feda1da78a8dcb205275fe2c7cbbdd7fa5abc34
sour <raphaella.c.ramos@gmail.com>
2022-02-21 20:34:30 -0500

    1

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-const myTitle = 'nighttime'
-let myObject = {'adjectives': ['sleepy', 'dark', 'comfy', 'foggy'], 'nouns': ['daze', 'bed', 'night', 'moon']}
-let myArticleArray = ['the', 'a']
+const myTitle = 'line by line'
+let myObject = {'lines': [
+  'a poem begins line by line',
+  'word by word',
+  'nouns as bricks',
+  'articles as mortar',
+  'a foundation of paper',
+  'blueprints of ink and graphite']}
+//let myArticleArray = ['The', 'an', 'a']
 
-function returnPoem(title, article, noun, adjective) {
-  return title + ': ' + article + ' ' + adjective + ' ' + noun
+function returnPoem(title, noun, adjective) {
+  return title + ': ' + adjective + ' ' + noun
 }
 
-W(returnPoem(myTitle, myArticleArray[0], myObject.adjectives[0],myObject.nouns[0]))
+//W(returnPoem(myTitle, myArticleArray[0], myObject.adjectives[0],myObject.nouns[0]))
 
-function forLoopPoem(title, articles, nouns, adjectives) {
+function forLoopPoem(title, lines) {
   W(title)
-  for (i = 0; i < 3; i++) {
-    W(articles[i] + ' ' + nouns[i] + ' ' + adjectives[i])
+  for (i = 0; i < 11; i++) {
+    W(lines[i])
   }
 }
 
-forLoopPoem(myTitle, myArticleArray, myObject.nouns, myObject.adjectives)
+//forLoopPoem(myTitle, myObject.lines)
 
 
 // while loop poem:
 let counter = 0
-while(counter < 5) {
-  W(getRandomItem(myArticleArray) + ' ' + getRandomItem(myObject.adjectives) + ' ' + getRandomItem(myObject.nouns))
+while(counter < 4) {
+  W(getRandomItem(myObject.lines))
   counter++
 }
 
